a9c490a27912b0c84653ca98412dfd389759147f
curkovic <milan.cure@outlook.com>
2022-06-26 20:01:12 +0200

    Kreiranje mapper-a, product mapper i service mapper

---
+++ ProductsAndServices/ProductsAndServices/MapperProfiles/ProductMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProductsAndServices.DTOs.ProductDTOs;
+using ProductsAndServices.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.MapperProfiles
+{
+    public class ProductMapper : Profile
+    {
+        public ProductMapper() {
+            CreateMap<Product, ProductReadDTO>();
+
+            CreateMap<ProductCreateDTO, Product>();
+        }
+
+    }
+}
---
+++ ProductsAndServices/ProductsAndServices/MapperProfiles/ServiceMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProductsAndServices.DTOs.ServiceDTOs;
+using ProductsAndServices.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.MapperProfiles
+{
+    public class ServiceMapper : Profile
+    {
+        public ServiceMapper()
+        {
+            CreateMap<Service, ServiceReadDTO>();
+
+            CreateMap<ServiceCreateDTO, Service>();
+        }
+    }
+}
