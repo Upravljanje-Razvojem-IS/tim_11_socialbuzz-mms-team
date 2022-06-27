1db756e4be27a6bbdd82205fa8eea33dabde5338
curkovic <milan.cure@outlook.com>
2022-06-27 12:17:23 +0200

    Kreiranje kontrolera sa osnovnim CRUD operacijama.

---
+++ ProductsAndServices/ProductsAndServices/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsAndServices.DTOs.ProductDTOs;
+using ProductsAndServices.Interfaces;
+
+namespace ProductsAndServices.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            var products = repository.GetAll();
+            return Ok(products); 
+        }
+
+        [HttpPost]
+        public ActionResult PostProduct(ProductCreateDTO productDTO)
+        {
+            var product = repository.CreateProduct(productDTO);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public ActionResult PutProduct(int id, ProductCreateDTO productDTO)
+        {
+            var product = repository.UpdateProduct(id, productDTO);
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteProduct(int id)
+        {
+            repository.DeleteProduct(id);
+            return Ok(new { message = "Uspesno obrisan proizvod!" });
+        }
+    }
+}
---
+++ ProductsAndServices/ProductsAndServices/Controllers/ServiceController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsAndServices.DTOs.ServiceDTOs;
+using ProductsAndServices.Interfaces;
+
+namespace ProductsAndServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServiceRepository repository;
+
+        public ServiceController(IServiceRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            var services = repository.GetAll();
+            return Ok(services);
+        }
+
+        [HttpPost]
+        public ActionResult PostService(ServiceCreateDTO serviceDTO)
+        {
+            var service = repository.CreateService(serviceDTO);
+            return Ok(service);
+        }
+
+        [HttpPut]
+        public ActionResult PutService(int id, ServiceCreateDTO serviceDTO)
+        {
+            var service = repository.UpdateService(id, serviceDTO);
+            return Ok(service);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteService(int id)
+        {
+            repository.DeleteService(id);
+            return Ok(new { message = "Uspesno obrisan servis!" });
+        }
+
+    }
+}
