a3fed777296b9450eedd4743fc2db0e658c5055d
curkovic <milan.cure@outlook.com>
2022-06-27 09:32:47 +0200

    Kreiranje interfejsa za proizvod i servis sa osnovnim metodama za CRUD operacije.

---
+++ ProductsAndServices/ProductsAndServices/Interfaces/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using ProductsAndServices.DTOs.ProductDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.Interfaces
+{
+    public interface IProductRepository
+    {
+        List<ProductReadDTO> GetAll();
+
+        ProductReadDTO GetById(int id);
+
+        ProductCreateDTO CreateProduct(ProductCreateDTO product);
+        ProductCreateDTO UpdateProduct(int id, ProductCreateDTO product);
+
+        void DeleteProduct(int id);
+
+        //dodatne metode
+    }
+}
---
+++ ProductsAndServices/ProductsAndServices/Interfaces/IServiceRepository.cs
@@ -0,0 +1,22 @@
+﻿using ProductsAndServices.DTOs.ServiceDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.Interfaces
+{
+    public interface IServiceRepository
+    {
+        List<ServiceReadDTO> GetAll();
+
+        ServiceReadDTO GetById(int id);
+
+        ServiceCreateDTO CreateService(ServiceCreateDTO service);
+        ServiceCreateDTO UpdateService(int id, ServiceCreateDTO service);
+
+        void DeleteService(int id);
+
+        //dodatne metode 
+    }
+}
