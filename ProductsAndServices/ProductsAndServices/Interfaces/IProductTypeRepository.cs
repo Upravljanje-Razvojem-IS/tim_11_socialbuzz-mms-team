90400d571a74838897f677fd5d8da9d70ce5ee0d
curkovic <milan.cure@outlook.com>
2022-07-04 00:08:16 +0200

    Kreiranje interfejsa za ProductType i ServiceType.

---
+++ ProductsAndServices/ProductsAndServices/Interfaces/IProductTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductsAndServices.DTOs.ProductTypeDTOs;
+using System.Collections.Generic;
+
+namespace ProductsAndServices.Interfaces
+{
+    public interface IProductTypeRepository
+    {
+        List<ProductTypeReadDTO> GetAllProductTypes();
+        ProductTypeCreateDTO CreateProductType(ProductTypeCreateDTO productTypeCreateDTO);
+        ProductTypeReadDTO UpdateProductType(int id, ProductTypeCreateDTO productTypeCreateDTO);
+
+        void DeleteProductType(int id);
+        ProductTypeReadDTO GetById(int id);
+
+        List<ProductTypeReadDTO> GetByDescription(string description);
+    }
+}
---
+++ ProductsAndServices/ProductsAndServices/Interfaces/IServiceTypeRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductsAndServices.DTOs.ServiceTypeDTOs;
+using System.Collections.Generic;
+
+namespace ProductsAndServices.Interfaces
+{
+    interface IServiceTypeRepository
+    {
+        List<ServiceTypeReadDTO> GetAllServiceTypes();
+
+        ServiceTypeCreateDTO CreateServiceType(ServiceTypeCreateDTO serviceTypeCreateDTO);
+        ServiceTypeCreateDTO UpdateServiceType(int id, ServiceTypeCreateDTO serviceTypeCreateDTO);
+
+        void DeleteProductType(int id);
+
+        ServiceTypeReadDTO GetById(int id);
+
+        List<ServiceTypeReadDTO> GetByDescription(string description);
+    }
+}
