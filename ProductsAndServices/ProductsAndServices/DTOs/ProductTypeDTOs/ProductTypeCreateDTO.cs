d4dccb15e4206cc1eb034160497332102c976f2d
curkovic <milan.cure@outlook.com>
2022-07-03 21:38:03 +0200

    Kreiranje DTO za ProductType, CreateDTO i ReadDTO.

---
+++ ProductsAndServices/ProductsAndServices/DTOs/ProductTypeDTOs/ProductTypeCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.DTOs.ProductTypeDTOs
+{
+    public class ProductTypeCreateDTO
+    {
+        public string Description { get; set; }
+    }
+}
