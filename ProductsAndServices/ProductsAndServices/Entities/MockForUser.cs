d0261dada441eca2dd97c7638e0641d1f36b0e51
curkovic <milan.cure@outlook.com>
2022-07-03 14:15:14 +0200

    Kreiranje Mock-a za User-a.

---
+++ ProductsAndServices/ProductsAndServices/Entities/MockForUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.Entities
+{
+    public class MockForUser
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public static class MockUserData
+    {
+        public static List<MockForUser> Users = new List<MockForUser>(){
+            new MockForUser()
+            {
+                FirstName = "Lazar",
+                LastName = "Lazarevic",
+                Email = "laza.lazarevic@gmail.com"
+            },
+
+            new MockForUser()
+            {
+                FirstName = "Milos",
+                LastName = "Milosevic",
+                Email = "milos.milosevic@yahoo.com"
+            }
+        };
+    }
+}
