5481cdbb775bb8e10afc92219b1d303b21ceab9f
curkovic <milan.cure@outlook.com>
2022-07-03 21:44:18 +0200

    Dodavanje ogranicenja entitetima Product i Service - (Pk, Fk, Required) i remove unnecessary 'using'.

--- ProductsAndServices/ProductsAndServices/Entities/Product.cs
+++ ProductsAndServices/ProductsAndServices/Entities/Product.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductsAndServices.Entities
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public int Quantity { get; set; }
 
+        [Required]
         public double Price { get; set; }
+
+        [ForeignKey("ProductTypeID")]
+        public int ProductTypeID { get; set; }
+
+        public virtual ProductType ProductType { get; set; }
+
     }
 }
--- ProductsAndServices/ProductsAndServices/Entities/Service.cs
+++ ProductsAndServices/ProductsAndServices/Entities/Service.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductsAndServices.Entities
 {
     public class Service
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public double Price { get; set; }
+
+        [ForeignKey("ServiceTypeID")]
+        public int ServiceTypeID { get; set; }
+
+        public virtual ServiceType ServiceType { get; set; }
     }
 }
