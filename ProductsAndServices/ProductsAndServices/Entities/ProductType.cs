04027e8379344b0fd295c4eb2d893a9e952c472c
curkovic <milan.cure@outlook.com>
2022-07-03 21:39:34 +0200

    Kreiranje entitea ProductType.

---
+++ ProductsAndServices/ProductsAndServices/Entities/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.Entities
+{
+    public class ProductType
+    {
+        public int ProductTypeID { get; set; }
+
+        public string Description { get; set; }
+    }
+}
