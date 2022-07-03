4a5449a27876ce32f884a610bf057b72ed1b6405
curkovic <milan.cure@outlook.com>
2022-07-03 21:50:08 +0200

    Kreiranje DTO za ServiceType, CreateDTO i ReadDTO.

---
+++ ProductsAndServices/ProductsAndServices/DTOs/ServiceTypeDTOs/ServiceTypeCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.DTOs.ServiceTypeDTOs
+{
+    public class ServiceTypeCreateDTO
+    {
+        public string Description { get; set; }
+    }
+}
