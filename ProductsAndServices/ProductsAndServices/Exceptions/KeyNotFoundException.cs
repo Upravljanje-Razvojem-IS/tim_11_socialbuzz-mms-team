d845a32b987b6ffe1880bfd06185d43b1996aa50
curkovic <milan.cure@outlook.com>
2022-07-04 21:15:52 +0200

    Kreiranje exception-a kada kljuc - Id nije pronadjen.

---
+++ ProductsAndServices/ProductsAndServices/Exceptions/KeyNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServices.Exceptions
+{
+    public class KeyNotFoundException : Exception
+    {
+        public KeyNotFoundException() : base() { }
+        public KeyNotFoundException(string message) : base(message) { }
+        public KeyNotFoundException(string message, params object[] args) : base(String.Format(CultureInfo.CurrentCulture, message, args)) { }
+
+    }
+}
