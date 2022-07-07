ac2d059bc9ef5f76047ee7bf8bcf221de78ecf10
djurovic <33664772+djurovic@users.noreply.github.com>
2022-07-07 14:55:30 +0200

    ss

--- UserService/UserService/DTOs/RoleDTOs/RoleCreateDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserService.DTOs.RoleDTOs
-{
-    public class RoleCreateDTO
-    {
-        public string RoleName { get; set; }
-
-    }
-}
--- UserService/UserService/DTOs/RoleDTOs/RoleReadDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserService.DTOs.RoleDTOs
-{
-    public class RoleReadDTO
-    {
-        public int RoleId { get; set; }
-        public string RoleName { get; set; }
-    }
-}
