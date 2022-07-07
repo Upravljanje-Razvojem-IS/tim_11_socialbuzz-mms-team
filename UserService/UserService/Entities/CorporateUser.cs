0e262e0542d942b1d93b1b3c1dfda949f409f813
djurovic <33664772+djurovic@users.noreply.github.com>
2022-07-07 14:49:48 +0200

    Try

--- UserService/UserService/Entities/CorporateUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserService.Entities
-{
-    public class CorporateUser: User
-    { 
-        public string CorporationName { get; set; }
-        public string PIB { get; set; }
-    }
-}
--- UserService/UserService/Entities/PersonalUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserService.Entities
-{
-    public class PersonalUser: User
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
